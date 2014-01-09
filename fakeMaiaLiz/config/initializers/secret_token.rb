f94abbb150e739c74ca2a7a3fb3d85b63565844e
lizetho <lizeth.olarte@gmail.com>
2014-01-09 15:51:54 +0100

    Inital Repository maia

---
+++ fakeMaiaLiz/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FakeMaiaLiz::Application.config.secret_key_base = '0d49c68403c40443982fb7538932a09297494774bee809b4b822e075836f58b1c95cfc4f753598852f8ed337fbec33bfa8b9ae084352e06bd6a259b61ab0f838'
